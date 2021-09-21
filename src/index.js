ec18b97cf5b5542ed426ce83d96c89977b8e7c0d
douglasromeiro <douglasromeiro320@gmail.com>
2021-09-21 19:20:50 -0300

    Revert "alidação cpf"
    This reverts commit ba56c8de52273cf0ddc5e4dfb6a136afc80f8c52.

--- src/index.js
+++ src/index.js
@@ -1,48 +1,30 @@
-const express = require("express");
-const { v4: uuidv4 } = require("uuid");
+const express = require('express');
+const { v4: uuidv4 } = require("uuid")
 
 const app = express();
 
 app.use(express.json());
 
-const customers = [];
+const costumers = [];
 
 /**
  * cpf - string
- * name - string
+ * name - string 
  * id - uuid
  * statment []
  */
 app.post("/account", (req, res) => {
-  const { cpf, name } = req.body;
-  //restorna um boolean
-  const customerAlreadyExists = customers.some(
-    (customers) => customers.cpf === cpf
-  );
+    const { cpf, name } = req.body;
 
-  if (customerAlreadyExists) {
-    return res.status(400).json({ error: "Customer already exists!" });
-  }
+    const id = uuidv4();
 
-  customers.push({
-    cpf,
-    name,
-    id: uuidv4(),
-    statment: [],
-  });
-  return res.status(201).send();
-});
-
-app.get("/statment", (req, res) => {
-  const { cpf } = req.headers;
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return res.status(400).json({ error: "Customer not found!" });
-  }
-
-  return res.json(customer.statment);
-});
+    costumers.push({
+        cpf,
+        name,
+        id,
+        statment: []
+    });
+    return res.status(201).send();
+})
 
 app.listen(8080);
