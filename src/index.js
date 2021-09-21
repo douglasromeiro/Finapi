ba56c8de52273cf0ddc5e4dfb6a136afc80f8c52
douglasromeiro <douglasromeiro320@gmail.com>
2021-09-21 19:16:48 -0300

    alidação cpf

--- src/index.js
+++ src/index.js
@@ -1,30 +1,48 @@
-const express = require('express');
-const { v4: uuidv4 } = require("uuid")
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 app.use(express.json());
 
-const costumers = [];
+const customers = [];
 
 /**
  * cpf - string
- * name - string 
+ * name - string
  * id - uuid
  * statment []
  */
 app.post("/account", (req, res) => {
-    const { cpf, name } = req.body;
+  const { cpf, name } = req.body;
+  //restorna um boolean
+  const customerAlreadyExists = customers.some(
+    (customers) => customers.cpf === cpf
+  );
 
-    const id = uuidv4();
+  if (customerAlreadyExists) {
+    return res.status(400).json({ error: "Customer already exists!" });
+  }
 
-    costumers.push({
-        cpf,
-        name,
-        id,
-        statment: []
-    });
-    return res.status(201).send();
-})
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statment: [],
+  });
+  return res.status(201).send();
+});
+
+app.get("/statment", (req, res) => {
+  const { cpf } = req.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(400).json({ error: "Customer not found!" });
+  }
+
+  return res.json(customer.statment);
+});
 
 app.listen(8080);
