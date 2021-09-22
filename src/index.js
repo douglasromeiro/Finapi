96e62c5bff8a859c51d215e066e0f04028f625e4
Douglas Ferreira <douglasromeiro320@gmail.com>
2021-09-22 15:49:56 -0300

    Atualizando middlware e adicionando requisição de deposito

--- src/index.js
+++ src/index.js
@@ -5,7 +5,22 @@ const app = express();
 
 app.use(express.json());
 
-const costumers = [];
+const customers = [];
+
+function verifyExistsAccountCpf(request, response, next){
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+
+    if(!customer){
+        return response.status(400).json({error: "Customer not found!"});
+    }
+
+    request.customer = customer;
+
+    return next();
+}
 
 /**
  * cpf - string
@@ -13,18 +28,41 @@ const costumers = [];
  * id - uuid
  * statment []
  */
-app.post("/account", (req, res) => {
-    const { cpf, name } = req.body;
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
 
     const id = uuidv4();
 
-    costumers.push({
+    customers.push({
         cpf,
         name,
         id,
         statment: []
     });
-    return res.status(201).send();
+    return response.status(201).send();
+});
+
+app.get("/statment", verifyExistsAccountCpf, (request, response) => {
+    const { customer } = request;
+
+    return response.json(customer.statment);
+});
+
+app.post("/deposit", verifyExistsAccountCpf, (request, response) =>{
+     const { description, amount} = request.body;
+
+     const { customer } = request;
+
+     const statmentOperation ={
+         description,
+         amount,
+         create_at: new Date(),
+         type: 'credit'
+     }
+
+     customer.statment.push(statmentOperation);
+
+     return response.status(201).send();
 })
 
 app.listen(8080);
